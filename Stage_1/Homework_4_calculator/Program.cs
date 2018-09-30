a9f4f9596f56f4e19d76c85e7f3b078b4988dc13
lusineduryan <lusineduryan@mail.ru>
2018-09-30 16:58:51 +0400

    added Homework_4_calculator

---
+++ Stage_1/Homework_4_calculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the first number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nPlease enter the second number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nPlease enter the operation type: ");
+            string operationType = Console.ReadLine();
+
+            double result = 0;
+            if (operationType == "+")
+            {
+                result = firstNumber + secondNumber;
+            }
+            else if (operationType == "-")
+            {
+                result = firstNumber - secondNumber;
+            }
+            else if (operationType == "*")
+            {
+                result = firstNumber * secondNumber;
+            }
+            else if (operationType == "/")
+            {
+                result = firstNumber / secondNumber;
+            }
+            else if (operationType == "%")
+            {
+                result = firstNumber % secondNumber;
+            }
+            else if (operationType == "^")
+            {
+                result = Math.Pow(firstNumber, secondNumber);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid operation type");
+            }
+            Console.WriteLine($"\nYour result is: {Math.Abs(result)}");
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
