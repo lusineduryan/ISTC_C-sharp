3db5853601b064ccad40e40bc6a4986c1ca71743
lusine duryan <lusineduryan@mail.ru>
2018-10-03 18:12:25 +0400

    Homework_5_caps added

---
+++ Homework_5_caps/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5_caps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the letter:");
+            char letter = Convert.ToChar(Console.ReadLine());
+           
+
+            Console.WriteLine(char.ToUpper(letter));
+            Console.ReadKey();
+        }
+    }
+}
