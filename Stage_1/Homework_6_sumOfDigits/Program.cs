7752ea5bb4072e30f57fe06c943c0bf52608cb94
lusine duryan <lusineduryan@mail.ru>
2018-10-05 14:12:38 +0400

    a check added

--- Stage_1/Homework_6_sumOfDigits/Program.cs
+++ Stage_1/Homework_6_sumOfDigits/Program.cs
@@ -12,14 +12,26 @@ namespace Homework_6_sumOfDigits
 
         {
             Console.WriteLine("Please enter the input number");
+            
             int inputNumber = Convert.ToInt32(Console.ReadLine());
-            int firstAdd = inputNumber / 1000;
-            int secondAdd = (inputNumber % 1000) / 100;
-            int thirdAdd = ((inputNumber % 1000) % 100) / 10;
-            int forthAdd = ((inputNumber % 1000) % 100) % 10;
-            int sum = firstAdd + secondAdd + thirdAdd + forthAdd;
+            int sum = 0;
+            bool flag = true;
+            if (inputNumber % 10000 ==0)
+            {
 
-            Console.WriteLine($"Sum of digits is\n{sum}");
+                int firstAdd = inputNumber / 1000;
+                int secondAdd = (inputNumber % 1000) / 100;
+                int thirdAdd = ((inputNumber % 1000) % 100) / 10;
+                int forthAdd = ((inputNumber % 1000) % 100) % 10;
+                sum = firstAdd + secondAdd + thirdAdd + forthAdd;
+            }
+            else
+            {
+                flag = false;
+            }
+            
+
+            Console.WriteLine(flag?$"Sum of digits is\n{sum}":"Please enter a number with 4 digits");
             Console.ReadKey();
 
         }
