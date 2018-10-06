28fe9555682fc66ec087885d57d19d9d754d4e0e
lusineduryan <lusineduryan@mail.ru>
2018-10-06 20:55:24 +0400

    Homework_7 re-arranging

---
+++ Stage_1/Homework_7_loops/Ex_4_1to9powerOfn/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_4_1to9powerOfn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine(Math.Pow(n,i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stage_1/Homework_7_loops/Ex_7_sumOfWholeNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_7_sumOfWholeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 6;
+            int B = 9;
+            int sum = 0;
+            for (int i = A; i <= B; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
