4de1347729f8ea33661e594f90425ccd72b41f1f
lusineduryan <lusineduryan@mail.ru>
2018-10-16 01:29:40 +0400

    Factorial added

---
+++ Stage_1/Homework_10_recursion/Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static int Factorial (int number)
+        {
+            if (number == 1) return 1;
+            return number * Factorial(number - 1);
+        }
+        static void Main(string[] args)
+        {
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+            int factorialResult = Factorial(inputNumber);
+            Console.WriteLine($"The factorial of {inputNumber} is {factorialResult}.");
+            Console.ReadKey();
+        }
+    }
+}
