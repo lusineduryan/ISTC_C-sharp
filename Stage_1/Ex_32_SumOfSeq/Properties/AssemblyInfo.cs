6dcf02be9b25c3059a681cf4b868f4a618aa30d7
lusineduryan <lusineduryan@mail.ru>
2018-10-22 09:45:30 +0400

    ex 31-33 added

--- Stage_1/Ex_32_SumOfSeq/Properties/AssemblyInfo.cs
+++ Stage_1/Ex_32_SumOfSeq/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ex_32_sumOfSeq1")]
+[assembly: AssemblyTitle("Ex_32_SumOfSeq")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ex_32_sumOfSeq1")]
+[assembly: AssemblyProduct("Ex_32_SumOfSeq")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6791d405-8138-4c38-9d28-edc03afc8c09")]
+[assembly: Guid("c6258162-6816-446d-9446-f5dda8755627")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Stage_1/Ex_33_SumOfSeq1/Program.cs
+++ Stage_1/Ex_33_SumOfSeq1/Program.cs
@@ -4,30 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ex_32_sumOfSeq1
+namespace Ex_33_SumOfSeq1
 {
     class Program
     {
-        static double ReverseFactorial (int number)
+        static double ReverseFactorial(int number)
         {
             if (number == 1) return 1;
-            return ReverseFactorial(number - 1) * (1.0 / (double) number);
+            return ReverseFactorial(number - 1) * (1.0 / number);
         }
-        static double SumOfSeq (int number)
+        static double SumOfSeq(double number, int value)
         {
             double sum = 1;
             for (int i = 1; i <= number; i++)
             {
-                sum += ReverseFactorial(i);
+                sum += (ReverseFactorial(i) * Math.Pow(value,i));
             }
+            Console.WriteLine(sum);
             return sum;
         }
         static void Main(string[] args)
         {
             int inputNumber = Convert.ToInt32(Console.ReadLine());
-            double rg = ReverseFactorial(inputNumber);
-            //double sum = SumOfSeq(inputNumber);
+            int inputValue = Convert.ToInt32(Console.ReadLine());
+            double sumOfSeq = SumOfSeq (inputNumber, inputValue);
             Console.ReadKey();
+            
         }
     }
 }
