2f286bb9b2ccf7fc7af4588b01912cbbf1186cef
lusine duryan <lusineduryan@mail.ru>
2018-11-14 15:14:40 +0400

    Abstract class created

---
+++ Stage_2/AbstractFinancialClass/AbstractFinancialInstrument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFinancialClass
+{
+    public abstract class AbstractFinancialInstrument
+    {
+        private string marketType;
+        private int tenor;
+
+
+    }
+}
