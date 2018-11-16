4f68c2a83f7e37bf1c5b699434d12bf8726409f6
lusine duryan <lusineduryan@mail.ru>
2018-11-16 18:17:43 +0400

    abstract class added

--- Stage_2/AbstractFinancialClass/AbstractFinancialInstrument.cs
+++ Stage_2/AbstractFinancialClass/AbstractFinancialInstrument.cs
@@ -11,6 +11,38 @@ namespace AbstractFinancialClass
         private string marketType;
         private int tenor;
 
+        public AbstractFinancialInstrument(string marketType, int tenor)
+        {
+            this.marketType = marketType;
+            this.tenor = tenor;
+        }
+
+        public string GetMarketType()
+        {
+            return marketType;
+        }
+
+        public int GetTenor()
+        {
+            return tenor;
+        }
+    }
+   
+      
+        public class LiquidInstrument : AbstractFinancialInstrument
+        {
+            public LiquidInstrument(string marketType, int tenor) : base(marketType, tenor)
+            {
+
+            }
+        }
+        public class ILLiquidInstrument : AbstractFinancialInstrument
+        {
+            public ILLiquidInstrument(string marketType, int tenor) : base(marketType, tenor)
+            {
+
+            }
+        }
 
     }
-}
+
--- Stage_2/AbstractFinancialClass/Program.cs
+++ Stage_2/AbstractFinancialClass/Program.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace AbstractFinancialClass
 {
-
-
     class Program
     {
         static void Main(string[] args)
         {
+            LiquidInstrument Equity = new LiquidInstrument("cash", 1);
+            ILLiquidInstrument ABS = new ILLiquidInstrument("capital", 30);
+            AbstractFinancialInstrument abstractInstrument = null;
+            int number = Convert.ToInt32(Console.ReadLine());
+            switch (number)
+            {
+                case 1: abstractInstrument = Equity; break;
+                case 2: abstractInstrument = ABS; break;
+                default: break;
+            }
+
+            abstractInstrument.GetType();
+            abstractInstrument.GetTenor();
         }
     }
 }
