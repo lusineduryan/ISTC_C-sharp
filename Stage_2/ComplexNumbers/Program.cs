d51b3ee33aaebc141b3b6e4da21722e5927b957d
lusine duryan <lusineduryan@mail.ru>
2018-11-21 18:29:54 +0400

    complex number partly added

---
+++ Stage_2/ComplexNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumbers
+{
+    class Program
+    {
+        struct ComplexNumber
+        {
+            private double realPart;
+            private double imaginaryPart;
+            public ComplexNumber(double realPart, double imaginaryPart)
+            {
+                this.realPart = realPart;
+                this.imaginaryPart = imaginaryPart;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
