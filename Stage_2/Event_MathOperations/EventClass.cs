353e6e390fd83b9b5bbd6b5fe21bd422dd2866f6
lusine duryan <lusineduryan@mail.ru>
2018-11-29 23:16:32 +0400

    event operations added

---
+++ Stage_2/Event_MathOperations/EventClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_MathOperations
+{
+    public class EventClass
+    {
+        public delegate int OperationDelegate(int number1, int number2);
+        public event OperationDelegate OperationsEvent;
+        public void Invoker()
+        {
+            if (OperationsEvent != null) OperationsEvent.Invoke(Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()));
+        }
+    }
+}
---
+++ Stage_2/Event_MathOperations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_MathOperations
+{
+    class Program
+    {
+        #region
+
+        public static int Sum (int number1, int number2)
+        {
+            Console.WriteLine($"Sum is {number1 + number2}");
+            return number1 + number2;
+        }
+        public static int Subtraction (int number1, int number2)
+        {
+            Console.WriteLine($"Subtraction is {number1 - number2}");
+            return number1 - number2;
+        }
+        public static int Product (int number1, int number2)
+        {
+            Console.WriteLine($"Product is {number1*number2}");
+            return number1 * number2;
+        }
+        public static int Division (int number1, int number2)
+        {
+            Console.WriteLine($"Division is {number1 / number2}");
+            if (number2 != 0) return number1 / number2; else return 0;
+        }
+        
+        #endregion
+
+        static void Main(string[] args)
+        {
+            EventClass eventclass = new EventClass();
+            eventclass.OperationsEvent += Sum;
+            eventclass.OperationsEvent += Subtraction;
+            eventclass.OperationsEvent += Product;
+            eventclass.OperationsEvent += Division;
+
+            eventclass.Invoker();
+            Console.ReadKey();
+        }
+    }
+}
