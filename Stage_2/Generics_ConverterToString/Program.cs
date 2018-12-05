c2167838bc75986cb9149a812b2ad736fc526315
lusine duryan <lusineduryan@mail.ru>
2018-12-05 17:43:35 +0400

    changes in generics

--- Stage_2/Generics_ConverterToString/Program.cs
+++ Stage_2/Generics_ConverterToString/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace Generics_ConverterToString
 {
+    class GenericConverter<T>
+    {
+        private T input;
+        public GenericConverter (T input)
+        {
+            this.input = input;
+        }
+
+        public string Converter<T>(T input)
+        {
+            string res = "";
+            try
+            {
+                res = input.ToString();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return res;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            GenericConverter<double> input1 = new GenericConverter<double>(10.5);
+            Console.WriteLine(input1.Converter(10.5));
+            GenericConverter<int> input2 = new GenericConverter<int>(20);
+            Console.WriteLine(input2.Converter(20));
+            Console.ReadKey();
         }
     }
 }
