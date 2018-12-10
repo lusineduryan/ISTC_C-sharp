c25f98a6a0417addba59d40d2c3a27902f3254a6
lusineduryan <lusineduryan@mail.ru>
2018-12-10 16:54:23 +0400

    extension methods added

---
+++ Stage_2/IndexerForList/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerForList
+{
+    class Listt
+    {
+        private int[] array = new int[5];
+        public int this[int index]
+        {
+            get
+            {
+                try
+                {
+                    return array[index];
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("out of the pre-defined range");
+                    return 0;
+                }
+
+            }
+
+            set
+            {
+                if (index <= array.Length)
+                {
+                    array[index] = value;
+                }
+                else
+                {
+                    array = new int[index];
+                }
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ListPrototype = new Listt();
+            ListPrototype[3] = 7;
+            try
+            {
+                Console.WriteLine(ListPrototype[Convert.ToInt32(Console.ReadLine())]);
+            }
+            catch (FormatException e)
+            {
+                throw;
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stage_2/MethodExtension/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.SecondStage.OOP.Extension01
+{
+    public static class Extensions
+    {
+        public static int SumOfDigits(this int i)
+        {
+            int sum = 0;
+            while (i != 0)
+            {
+                sum += i % 10;
+                i /= 10;
+            }
+            return sum;
+        }
+
+        public static int NumberOfAs(this string str)
+        {
+            int count = 0;
+            char[] str1 = str.ToCharArray();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str1[i] == 'a')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 234;
+            int sum = number.SumOfDigits();
+            Console.WriteLine(sum);
+            string str = "apple";
+            int count = str.NumberOfAs();
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
