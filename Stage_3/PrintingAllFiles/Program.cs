2397507e8f9f08b082e5d0ff2a90da26ad73f84d
lusine duryan <lusineduryan@mail.ru>
2018-12-25 19:25:27 +0400

    print all files added

---
+++ Stage_3/PrintingAllFiles/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PrintingAllFiles
+{
+    class Program
+    {
+        public static void PrintAllFiles (string directoryName)
+        {
+            string[] directories = Directory.GetDirectories(directoryName);
+            string[] files = Directory.GetFiles(directoryName);
+
+            try
+            {
+                foreach (string directory in directories)
+                {
+                    Console.WriteLine(directory);
+                    PrintAllFiles(directory);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            string[] drives = Directory.GetLogicalDrives();
+
+            foreach (string drive in drives)
+            {
+                Console.WriteLine(drive);
+            }
+
+            string generalDrive = Console.ReadLine().ToUpper();
+            generalDrive += ":\\";
+
+            if (drives.Contains(generalDrive))
+            {
+                PrintAllFiles(generalDrive);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid directory name!");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
