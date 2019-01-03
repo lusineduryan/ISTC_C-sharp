1ebdff69870a1a55aff83217a68df8c8c3b08776
lusine duryan <lusineduryan@mail.ru>
2019-01-03 21:54:42 +0400

    brackets added

---
+++ Stage_3/Brackets/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brackets
+{
+    class Program
+    {
+        public static bool IsCorrect(string inputString)
+        {
+            var brackets = new List<char>(); 
+            foreach (char item in inputString)
+            {
+                switch (item)
+                {
+                    case '(':
+                        brackets.Add('(');
+                        break;
+                    case '[':
+                        brackets.Add('[');
+                        break;
+                    case '{':
+                        brackets.Add('{');
+                        break;
+                    case '<':
+                        brackets.Add('<');
+                        break;
+                    case ')':
+                        if (brackets.Count != 0 && brackets[brackets.Count-1] == '(')
+                        {
+                            brackets.RemoveAt(brackets.Count-1);
+                        }
+                        else return false;
+                        break;
+                    case ']':
+                        if (brackets.Count != 0 && brackets[brackets.Count - 1] == '[')
+                        {
+                            brackets.RemoveAt(brackets.Count - 1);
+                        }
+                        else return false;
+                        break;
+                    case '}':
+                        if (brackets.Count != 0 && brackets[brackets.Count - 1] == '{')
+                        {
+                            brackets.RemoveAt(brackets.Count - 1);
+                        }
+                        else return false;
+                        break;
+                    case '>':
+                        if (brackets.Count != 0 && brackets[brackets.Count - 1] == '<')
+                        {
+                            brackets.RemoveAt(brackets.Count - 1);
+                        }
+                        else return false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return brackets.Count == 0;
+        }
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool answer = IsCorrect(input);
+            Console.WriteLine(answer);
+            Console.ReadKey();
+        }
+    }
+}
