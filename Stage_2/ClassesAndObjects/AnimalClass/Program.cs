1acb35f2ebf5f7f2f1eb9a480d49d371cd8c25d0
lusine duryan <lusineduryan@mail.ru>
2019-07-03 16:00:12 +0400

    animal class added

---
+++ Stage_2/ClassesAndObjects/AnimalClass/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalClass
+{
+    class Animal
+    {
+        public static string Type { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+        public float Weight { get; set; }
+
+        static Animal()
+        {
+            Type = "Animal";
+        }
+
+        public Animal(int age)
+        {
+            Age = age;
+        }
+
+        public Animal(string color, int age): this(age)
+        {
+            Color = color;
+        }
+
+        public Animal(float weight, string color, int age): this(color, age)
+        {
+            Weight = weight;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Animal.Type);
+            Animal.Type = "animal";
+            Console.WriteLine(Animal.Type);
+            Animal Dog = new Animal(20.5f, "white", 3);
+            Console.WriteLine(Dog.Age);
+            Console.ReadKey();
+        }
+    }
+}
