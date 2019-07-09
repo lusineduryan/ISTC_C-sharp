68f839cd9a3cef99c08c1c721e98d71c84ccb80d
lusine duryan <lusineduryan@mail.ru>
2019-07-09 18:47:40 +0400

    inheritance added

---
+++ Stage_2/Hierarchy/Classes.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hierarchy
+{
+    public class Animal
+    {
+        private string name;
+        private int age;
+        private string meal;
+        public Animal(string name, int age, string meal)
+        {
+            this.name = name;
+            this.age = age;
+            this.meal = meal;
+        }
+    }
+    public class Herbivore : Animal
+    {
+        public void IsEating()
+        {
+            Console.WriteLine("I eat only plants");
+        }
+        public Herbivore(string name, int age, string meal) : base(name, age, "plants")
+        {
+        }
+    }
+    public class Carnivore : Animal
+    {
+        public void IsEating()
+        {
+            Console.WriteLine("I eat only meat");
+        }
+        public Carnivore(string name, int age, string meal) : base(name, age, "meat")
+        {
+        }
+    }
+    public class Omnivore : Animal
+    {
+        public void IsEating()
+        {
+            Console.WriteLine("I eat both plants and meat");
+        }
+        public Omnivore(string name, int age, string meal) : base(name, age, "plants & meat")
+        {
+        }
+    }
+    public class Rabbit : Herbivore
+    {
+        private string furColor;
+        public void Say()
+        {
+            Console.WriteLine("I run very fast!");
+        }
+        public Rabbit(string name, int age, string meal, string furColor) : base(name, age, meal)
+        {
+            this.furColor = furColor;
+        }
+    }
+    public class Lion : Carnivore
+    {
+        private double roarAmplitude;
+        public void Say()
+        {
+            Console.WriteLine("I am the king of all animals!");
+        }
+        public Lion(string name, int age, string meal, double roarAmplitude) : base(name, age, meal)
+        {
+            this.roarAmplitude = roarAmplitude;
+        }
+    }
+    public class Hyena : Carnivore
+    {
+        private double laughAmplitude;
+        public void Say()
+        {
+            Console.WriteLine("I am very dangerous!");
+        }
+        public Hyena(string name, int age, string meal, double laughAmplitude) : base(name, age, meal)
+        {
+            this.laughAmplitude = laughAmplitude;
+        }
+    }
+    public class Man : Omnivore
+    {
+        public bool HasConscious { get; set; } = true;
+        public void Say()
+        {
+            Console.WriteLine("I am homo spaiens!");
+        }
+        public Man(string name, int age, string meal, bool hasConscious) : base(name, age, meal)
+        {
+            HasConscious = hasConscious;
+        }
+    }
+}
--- Stage_2/Hierarchy/Program.cs
+++ Stage_2/Hierarchy/Program.cs
@@ -6,105 +6,12 @@ using System.Threading.Tasks;
 
 namespace Hierarchy
 {
- 
+
     class Program
     {
         static void Main(string[] args)
         {
 
-        }
-    }
-    class Animal
-    {
-        public string Name { get; set; }
-        public Animal(string name)
-        {
-            Name = name;
-        }
-    }
-    class Herbivore : Animal
-    {
-        public string MealP { get; set; }
-        public void EatsPlants()
-        {
-            Console.WriteLine("I eat only plants");
-        }
-        public Herbivore(string name, string meal) : base(name)
-        {
-            MealP = meal;
-        }
-    }
-    class Carnivore : Animal
-    {
-        public string MealM { get; set; }
-        public void Eatsmeat()
-        {
-            Console.WriteLine("I eat only meat");
-        }
-        public Carnivore(string name, string meal) : base(name)
-        {
-            MealM = meal;
-        }
-    }
-    class Omnivore : Animal
-    {
-        public string MealA { get; set; }
-        public void Eatsmeat()
-        {
-            Console.WriteLine("I eat both plants and meat");
-        }
-        public Omnivore(string name, string meal) : base(name)
-        {
-            MealA = meal;
-        }
-    }
-    class Rabbit : Herbivore
-    {
-        public string FurColor { get; set; }
-        public void Say()
-        {
-            Console.WriteLine("I run very fast!");
-        }
-        public Rabbit(string name, string meal, string fur) : base(name, meal)
-        {
-            FurColor = fur;
-        }
-
-    }
-    class Lion : Carnivore
-    {
-        public double RoarAmplitude { get; set; }
-        public void Say()
-        {
-            Console.WriteLine("I am the king of all animals");
-        }
-        public Lion(string name, string meal, double roar):base(name, meal)
-        {
-            RoarAmplitude = roar;
-        }
-    }
-    class Hyena : Carnivore
-    {
-        public double LaughAmplitude { get; set; }
-        public void Say()
-        {
-            Console.WriteLine("I am very dangerous");
-        }
-        public Hyena(string name, string meal, double laugh) : base(name, meal)
-        {
-            LaughAmplitude = laugh;
-        }
-    }
-    class Man : Omnivore
-    {
-        public bool HasConscious { get; set; } = true;
-        public void Say()
-        {
-            Console.WriteLine("I am homo spaiens");
-        }
-        public Man(string name, string meal, bool conscious) : base(name, meal)
-        {
-            HasConscious = conscious;
         }
     }
 }
