97314cd5ff3552a5adc499b94a14ca90ff75f48b
lusine duryan <lusineduryan@mail.ru>
2019-07-25 19:07:49 +0400

    objects methods overriding done

---
+++ Stage_2/ObjectMethodsOverriding/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectMethodsOverriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangular rect1 = new Rectangular(4, 6.5);
+            Rectangular rect2 = new Rectangular(4, 6);
+            Rectangular rect3 = new Rectangular(3, 8);
+
+            Console.WriteLine(rect2.Equals(rect3));
+            Console.WriteLine(rect1.ToString());
+            Console.WriteLine(rect1.GetHashCode());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stage_2/ObjectMethodsOverriding/Rectangular.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectMethodsOverriding
+{
+    class Rectangular
+    {
+        public double length;
+        public double width;
+
+        public Rectangular(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Rectangular rect = obj as Rectangular;
+            if (this.length * this.width == rect.length * rect.width) return true;
+            else return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)Math.Pow(this.length * this.width, 3);
+        }
+
+        public override string ToString()
+        {
+            return $"The rectangular has {width} width and {length} length.";
+        }
+    }
+}
