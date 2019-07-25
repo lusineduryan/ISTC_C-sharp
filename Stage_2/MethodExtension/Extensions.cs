1c49022a9aa7bdfbd80f5a6087886672b25d2c59
lusine duryan <lusineduryan@mail.ru>
2019-07-25 19:50:59 +0400

    extensions added

---
+++ Stage_2/MethodExtension/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExtension
+{
+    public static class Extensions
+    {
+        public static int SumOfDigits(this int i)
+        {
+            int sum = 0;
+            while (i != 0)
+            {
+                sum += i % 10;
+                i /= 10;
+            }
+            return sum;
+        }
+
+        public static int NumberOfAs(this string str)
+        {
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
--- Stage_2/MethodExtension/Program.cs
+++ Stage_2/MethodExtension/Program.cs
@@ -4,43 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ISTC.SecondStage.OOP.Extension01
+namespace MethodExtension
 {
-    public static class Extensions
-    {
-        public static int SumOfDigits(this int i)
-        {
-            int sum = 0;
-            while (i != 0)
-            {
-                sum += i % 10;
-                i /= 10;
-            }
-            return sum;
-        }
-
-        public static int NumberOfAs(this string str)
-        {
-            int count = 0;
-            char[] str1 = str.ToCharArray();
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (str1[i] == 'a')
-                {
-                    count++;
-                }
-            }
-            return count;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            int number = 234;
+            int number = Convert.ToInt32(Console.ReadLine());
             int sum = number.SumOfDigits();
             Console.WriteLine(sum);
-            string str = "apple";
+            string str = Console.ReadLine();
             int count = str.NumberOfAs();
             Console.WriteLine(count);
             Console.ReadKey();
