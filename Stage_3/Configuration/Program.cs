65cfb6f5a5538828547125890207d2f6751dd69f
lusine duryan <lusineduryan@mail.ru>
2019-08-19 04:32:08 +0400

    config added

---
+++ Stage_3/Configuration/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
+using System.Collections.Specialized;
+
+
+namespace ConfigTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = ConfigurationSettings.AppSettings["name"];
+            string myAge = ConfigurationSettings.AppSettings.Get("age");
+            var all = ConfigurationSettings.AppSettings;
+            foreach (var item in all.AllKeys)
+            {
+                Console.WriteLine($"Key: {item}, Value: {all.Get(item)} \n");
+            }
+
+            Console.WriteLine(myName);
+            Console.WriteLine(myAge);
+            Console.ReadKey();
+        }
+    }
+}
