a76cb843a655cd8bd93cfc3e531702337f99597f
lusine duryan <lusineduryan@mail.ru>
2019-08-22 16:36:07 +0400

    homework1 added

---
+++ Stage_3/NearestNumber/Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NearestNumber
+{
+    class Numbers
+    {
+        public static double FindNearestNumber(double[] numbers, double input)
+        {
+            if (input >= numbers[numbers.Length - 1])
+            {
+                return numbers[numbers.Length - 1];
+            }
+            double res = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                double prevDif = Math.Abs(input - numbers[i - 1]);
+                if (Math.Abs(input - numbers[i]) > prevDif)
+                {
+                    res =  numbers[i - 1];
+                    break;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Stage_3/NearestNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NearestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbersArray = { 1.2, 1.5, 2.5, 2.8, 3, 3.9, 8, 10, 15, 20, 25 };
+            Console.Write("Please input a number between 1 and 30: ");
+            double inputNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Numbers.FindNearestNumber(numbersArray, inputNumber));
+            Console.ReadKey();
+        }
+    }
+}
