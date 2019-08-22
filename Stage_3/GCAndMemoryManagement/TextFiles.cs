dbac8a0a3ab067c006c2a67ea6ac0b7bc59b30ef
lusine duryan <lusineduryan@mail.ru>
2019-08-22 18:18:19 +0400

    GC started

---
+++ Stage_3/GCAndMemoryManagement/TextFiles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GCAndMemoryManagement
+{
+    class TextFiles : IDisposable
+    {
+        private void ReleaseUnmanagedResources()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
