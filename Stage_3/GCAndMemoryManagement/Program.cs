6d98ec9a8ab859aaea5192ce16f10502868399cf
lusine duryan <lusineduryan@mail.ru>
2019-08-23 15:22:01 +0400

    final commit

--- Stage_3/GCAndMemoryManagement/Program.cs
+++ Stage_3/GCAndMemoryManagement/Program.cs
@@ -15,19 +15,21 @@ namespace GCAndMemoryManagement
             long memory = 0;
             using (TextFiles textFile = new TextFiles())
             {
-                StreamWriter file = textFile.CreateTextFileW(ConfigurationManager.AppSettings["path"], "Lucy.txt", out memory);
-                Console.Write("Please write a text in the file: ");
-                string inputText = Console.ReadLine();
-                textFile.WriteInTextFile(inputText, file, out memory);
-                file.Dispose();
+                using (StreamWriter file = textFile.CreateTextFileW(ConfigurationManager.AppSettings["path"], "Lucy.txt", out memory))
+                {
+                    Console.Write("Please write a text in the file: ");
+                    string inputText = Console.ReadLine();
+                    textFile.WriteInTextFile(inputText, file, out memory);
+                }
             }
             using (TextFiles textFile = new TextFiles())
             {
-                StreamReader file = textFile.CreateTextFileR(ConfigurationManager.AppSettings["path"] + "\\Lucy.txt", out memory);
-                string text = textFile.ReadFromTextFile(file, out memory);
-                Console.WriteLine($"The following text retrieved from the file: {text}");
-                Console.WriteLine($"Modified text is: {textFile.NewText(text)}");
-                file.Dispose();
+                using (StreamReader file = textFile.CreateTextFileR(ConfigurationManager.AppSettings["path"] + "\\Lucy.txt", out memory))
+                {
+                    string text = textFile.ReadFromTextFile(file, out memory);
+                    Console.WriteLine($"The following text retrieved from the file: {text}");
+                    Console.WriteLine($"Modified text is: {textFile.NewText(text)}");
+                }
             }
             Console.ReadKey();
         }
