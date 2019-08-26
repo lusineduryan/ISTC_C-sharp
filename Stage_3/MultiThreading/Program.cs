d77066777f353c2816dd3bd9c1458c2f75eb50e6
lusine duryan <lusineduryan@mail.ru>
2019-08-26 14:37:15 +0400

    task added

---
+++ Stage_3/MultiThreading/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task[] array = new Task[5];
+            TaskBuilding task = new TaskBuilding();
+            task.BuidTasks(array);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stage_3/MultiThreading/TaskBuilding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    public class TaskBuilding
+    {
+        public double Square(double arg)
+        {
+            return arg * arg;
+        }
+
+        public void BuidTasks(Task[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Task.Factory.StartNew
+                    (
+                        () =>
+                        {
+                            Console.WriteLine($"{Square(i)} - {Thread.CurrentThread.ManagedThreadId}");
+                        }
+                    );
+            }
+        }
+    }
+}
