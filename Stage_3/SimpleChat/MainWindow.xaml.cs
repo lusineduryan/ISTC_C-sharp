e054fd4cead3584b219e8a0b9da5b346ef60ec64
lusine duryan <lusineduryan@mail.ru>
2019-09-02 03:10:20 +0400

    chat added

---
+++ Stage_3/SimpleChat/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleChat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void AnnaMsg(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Anna texted: {Anna.Text}");
+        }
+
+        public void ChloeMsg(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Chloe texted: {Chloe.Text}");
+        }
+
+        public void Chat(object sender, EventArgs e)
+        {
+            MessageBox.Show("Conversation started!");
+        }
+
+    }
+}
