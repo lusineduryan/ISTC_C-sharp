5844276d5bbfce657f3eb21cbce6d07caf800b3b
lusine duryan <lusineduryan@mail.ru>
2019-09-04 14:19:38 +0400

    Linq methods added

---
+++ Stage_3/LINQ/ExtensionMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public static class ExtensionMethods
+    {
+        public static IEnumerable<object> Select<TSource>(this IEnumerable<TSource> source, Func<TSource, object> predicate)
+        {
+            foreach (TSource item in source)
+            {
+                yield return item;
+            }
+        }
+
+        public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (TSource item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static TSource FirstOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (TSource item in source)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+                else
+                {
+                    source.GetEnumerator().MoveNext();
+                }
+            }
+            return default(TSource);
+        }
+    }
+}
---
+++ Stage_3/LINQ/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Anna", "Ani", "Lilit", "Gohar", "Artyom", "Arpi", "Levon" };
+            var ANames = names.Where(s => s.StartsWith("A")).Select(s => s);
+            foreach (var item in ANames)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+            var LName = names.FirstOrDefault(s => s.StartsWith("L"));
+            Console.WriteLine(LName);
+            Console.ReadKey();
+        }
+    }
+}
