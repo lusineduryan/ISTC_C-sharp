78c22973794059c381ef68b5c958fe7702727fc5
lusine duryan <lusineduryan@mail.ru>
2019-09-05 15:58:19 +0400

    operators overloaded

---
+++ Stage_3/GenericBubbleSorting/Operators.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBubbleSorting
+{
+    public struct Operators
+    {
+        public static Operators operator +(Operators arg1, Operators arg2)
+        {
+            try
+            {
+                return arg1 + arg2;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Input type is not supported!");
+                return new Operators();
+            }
+        }
+
+        public static Operators operator -(Operators arg1, Operators arg2)
+        {
+            try
+            {
+                return arg1 - arg2;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Input type is not supported!");
+                return new Operators();
+            }
+        }
+
+        public static bool operator <(Operators arg1, Operators arg2)
+        {
+            try
+            {
+                if (arg1 < arg2) return true;
+                else return false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Input type is not supported!");
+                return false;
+            }
+        }
+
+        public static bool operator >(Operators arg1, Operators arg2)
+        {
+            try
+            {
+                if (arg1 > arg2) return true;
+                else return false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Input type is not supported!");
+                return false;
+            }
+        }
+    }
+}
--- Stage_3/GenericBubbleSorting/Sorting.cs
+++ Stage_3/GenericBubbleSorting/Sorting.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace GenericBubbleSorting
 {
-    public class Sorting<T>
+    public class Sorting
     {
+        public static Sorting operator +(arg1, arg2)
+        {
+            try
+            {
+                return arg1 + arg2;
+            }
+            catch (Exception)
+            {
 
+                throw;
+            }
+        }
+
+        private static void Swap<T>(ref T arg1, ref T arg2)
+        {
+            arg1 = arg1 + arg2;
+
+        }
     }
 }
