47d43e551262ab679b7624ae7903f6437172c02a
lusine duryan <lusineduryan@mail.ru>
2019-09-08 22:59:59 +0400

    first 2 tasks done

---
+++ Stage_3/GithubAnalyzer/Models.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubAnalyzer
+{
+    public class TotalInfo
+    {
+        public int total_count { get; set; }
+        public bool incomplete_results { get; set; }
+        public UserInfo[] items { get; set; }
+    }
+
+    public class UserInfo
+    {
+        public string login { get; set; }
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string avatar_url { get; set; }
+        public string gravatar_id { get; set; }
+        public string url { get; set; }
+        public string html_url { get; set; }
+        public string followers_url { get; set; }
+        public string following_url { get; set; }
+        public string gists_url { get; set; }
+        public string starred_url { get; set; }
+        public string subscriptions_url { get; set; }
+        public string organizations_url { get; set; }
+        public string repos_url { get; set; }
+        public string events_url { get; set; }
+        public string received_events_url { get; set; }
+        public string type { get; set; }
+        public bool site_admin { get; set; }
+        public float score { get; set; }
+    }
+
+
+    public class UserExtended
+    {
+        public string login { get; set; }
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string avatar_url { get; set; }
+        public string gravatar_id { get; set; }
+        public string url { get; set; }
+        public string html_url { get; set; }
+        public string followers_url { get; set; }
+        public string following_url { get; set; }
+        public string gists_url { get; set; }
+        public string starred_url { get; set; }
+        public string subscriptions_url { get; set; }
+        public string organizations_url { get; set; }
+        public string repos_url { get; set; }
+        public string events_url { get; set; }
+        public string received_events_url { get; set; }
+        public string type { get; set; }
+        public bool site_admin { get; set; }
+        public object name { get; set; }
+        public object company { get; set; }
+        public string blog { get; set; }
+        public string location { get; set; }
+        public object email { get; set; }
+        public object hireable { get; set; }
+        public object bio { get; set; }
+        public int public_repos { get; set; }
+        public int public_gists { get; set; }
+        public int followers { get; set; }
+        public int following { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+    }
+
+    public class RepoInfo
+    {
+        public Class1[] Property1 { get; set; }
+    }
+
+    public class Class1
+    {
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string name { get; set; }
+        public string full_name { get; set; }
+        public bool _private { get; set; }
+        public Owner owner { get; set; }
+        public string html_url { get; set; }
+        public string description { get; set; }
+        public bool fork { get; set; }
+        public string url { get; set; }
+        public string forks_url { get; set; }
+        public string keys_url { get; set; }
+        public string collaborators_url { get; set; }
+        public string teams_url { get; set; }
+        public string hooks_url { get; set; }
+        public string issue_events_url { get; set; }
+        public string events_url { get; set; }
+        public string assignees_url { get; set; }
+        public string branches_url { get; set; }
+        public string tags_url { get; set; }
+        public string blobs_url { get; set; }
+        public string git_tags_url { get; set; }
+        public string git_refs_url { get; set; }
+        public string trees_url { get; set; }
+        public string statuses_url { get; set; }
+        public string languages_url { get; set; }
+        public string stargazers_url { get; set; }
+        public string contributors_url { get; set; }
+        public string subscribers_url { get; set; }
+        public string subscription_url { get; set; }
+        public string commits_url { get; set; }
+        public string git_commits_url { get; set; }
+        public string comments_url { get; set; }
+        public string issue_comment_url { get; set; }
+        public string contents_url { get; set; }
+        public string compare_url { get; set; }
+        public string merges_url { get; set; }
+        public string archive_url { get; set; }
+        public string downloads_url { get; set; }
+        public string issues_url { get; set; }
+        public string pulls_url { get; set; }
+        public string milestones_url { get; set; }
+        public string notifications_url { get; set; }
+        public string labels_url { get; set; }
+        public string releases_url { get; set; }
+        public string deployments_url { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public DateTime pushed_at { get; set; }
+        public string git_url { get; set; }
+        public string ssh_url { get; set; }
+        public string clone_url { get; set; }
+        public string svn_url { get; set; }
+        public string homepage { get; set; }
+        public int size { get; set; }
+        public int stargazers_count { get; set; }
+        public int watchers_count { get; set; }
+        public string language { get; set; }
+        public bool has_issues { get; set; }
+        public bool has_projects { get; set; }
+        public bool has_downloads { get; set; }
+        public bool has_wiki { get; set; }
+        public bool has_pages { get; set; }
+        public int forks_count { get; set; }
+        public object mirror_url { get; set; }
+        public bool archived { get; set; }
+        public bool disabled { get; set; }
+        public int open_issues_count { get; set; }
+        public object license { get; set; }
+        public int forks { get; set; }
+        public int open_issues { get; set; }
+        public int watchers { get; set; }
+        public string default_branch { get; set; }
+    }
+
+    public class Owner
+    {
+        public string login { get; set; }
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string avatar_url { get; set; }
+        public string gravatar_id { get; set; }
+        public string url { get; set; }
+        public string html_url { get; set; }
+        public string followers_url { get; set; }
+        public string following_url { get; set; }
+        public string gists_url { get; set; }
+        public string starred_url { get; set; }
+        public string subscriptions_url { get; set; }
+        public string organizations_url { get; set; }
+        public string repos_url { get; set; }
+        public string events_url { get; set; }
+        public string received_events_url { get; set; }
+        public string type { get; set; }
+        public bool site_admin { get; set; }
+    }
+}
---
+++ Stage_3/GithubAnalyzer/Process.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace GithubAnalyzer
+{
+    public class Process
+    {
+        public static List<UserInfo> GetWebData()
+        {
+            WebClient source = new WebClient();
+            string content = "";
+            List<UserInfo> users = new List<UserInfo>();
+            for (int i = 0; i < 10; i++)
+            {
+                content = source.DownloadString("https://api.github.com//search//users?q=location:armenia&page=" + i + "&per_page=100");
+                var model1 = JsonConvert.DeserializeObject<TotalInfo>(content);
+                users.AddRange(model1.items);
+            }
+            return users;
+        }
+
+        public static List<UserExtended> GetUserData()
+        {
+            WebClient source = new WebClient();
+            List<UserInfo> users = GetWebData();
+            List<UserExtended> profiles = new List<UserExtended>();
+            for (int i = 0; i < 9; i++)
+            {
+                string urls = source.DownloadString(users[i].url);
+                profiles = JsonConvert.DeserializeObject<List<UserExtended>>(urls);
+            }
+            return profiles;
+        }
+
+        public static List<RepoInfo> GetRepoData()
+        {
+            WebClient source = new WebClient();
+            List<UserInfo> users = GetWebData();
+            List<RepoInfo> repos = new List<RepoInfo>();
+            for (int i = 0; i < 9; i++)
+            {
+                string repoUrls = source.DownloadString(users[i].repos_url);
+                repos = JsonConvert.DeserializeObject<List<RepoInfo>>(repoUrls);
+            }
+            return repos;
+        }
+    }
+}
