7379699d8bd4704bd03f7adbc2163834e67e8703
lusine duryan <lusineduryan@mail.ru>
2019-09-23 15:05:48 +0400

    DML added

---
+++ Stage_4/DML/BasicCommands.sql
@@ -0,0 +1,10 @@
+use Monitoring
+select * from Company
+
+select Industry, count(Industry) from Company
+where Industry is not null and len(Industry) > 1
+group by Industry
+
+select [Name], DateOfFoundation from Company
+where Industry = 'Information technologies' and DateOfFoundation > 2010
+order by DateOfFoundation desc
