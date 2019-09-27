bbe63e252a1520855f4ab2bb2b2eddbe7d1ea36e
lusine duryan <lusineduryan@mail.ru>
2019-09-27 18:11:20 +0400

    b

--- Stage_4/DML/BasicCommands.sql
+++ Stage_4/DML/BasicCommands.sql
@@ -5,6 +5,8 @@ select Industry, count(Industry) from Company
 where Industry is not null and len(Industry) > 1
 group by Industry
 
+
 select [Name], DateOfFoundation from Company
 where Industry = 'Information technologies' and DateOfFoundation > 2010
 order by DateOfFoundation desc
+
