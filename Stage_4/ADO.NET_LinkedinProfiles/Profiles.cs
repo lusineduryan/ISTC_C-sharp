1c2d75e7e9ffb92cb0a913a5a8fd910a19cd979c
lusine duryan <lusineduryan@mail.ru>
2019-10-01 23:54:37 +0400

    f

---
+++ Stage_4/ADO.NET_LinkedinProfiles/Profiles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_LinkedinProfiles
+{
+    public class Profiles
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string Speciality { get; set; }
+        public string Location { get; set; }
+        public string Education { get; set; }
+        public string Company { get; set; }
+        public int? ConnectionCount { get; set; }
+        public string Website { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Connected { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
---
+++ Stage_4/ADO.NET_LinkedinProfiles/SQLConnector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace ADO.NET_LinkedinProfiles
+{
+    public class SQLConnector
+    {
+        public static List<Profiles> GetProfilesInfo()
+        {
+            List<Profiles> profiles = new List<Profiles>();
+            SqlConnectionStringBuilder connectionBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = @"(localDB)DESKTOP-0IKC9M0",
+                InitialCatalog = "Monitoring",
+                IntegratedSecurity = true
+            };
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionBuilder.ConnectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine(connection.State);
+                    string query = "select * from LinkedinProfiles";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader readData = command.ExecuteReader())
+                        {
+                            while (readData.Read())
+                            {
+                                profiles.Add(
+                                    new Profiles
+                                    {
+                                        Id = (int)readData["ID"],
+                                        Username = (string)
+                                    }
+                                    );
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return profiles;
+        }
+    }
+}
