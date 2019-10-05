e3c23f8d87ffcf939f45837617ca0d10d389e962
lusine duryan <lusineduryan@mail.ru>
2019-10-05 22:23:31 +0400

    gg

--- Stage_4/EntityFramework_LinkedinProfiles/NewModel.cs
+++ Stage_4/EntityFramework_LinkedinProfiles/NewModel.cs
@@ -12,6 +12,7 @@ namespace EntityFramework_LinkedinProfiles
         {
             using (MonitoringEntities context = new MonitoringEntities())
             {
+
                 return context.GithubLinkedinCrossTables.Join(context.GithubProfiles, x => x.GithubUserId, y => y.Id, (x, y) => new { ID = y.Id, LinkedinUserId = x.LinkedinUserId, GithubUsername = y.UserName }).Join(context.LinkedinProfiles, x => x.LinkedinUserId, y => y.Id, (x, y) => new { Id = x.ID, GithubUsername = x.GithubUsername, LinkedinUsername = y.Username }).Select(x => new { x.Id, x.GithubUsername, x.LinkedinUsername }).ToList();
             }
         }
