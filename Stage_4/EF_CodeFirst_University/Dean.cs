71b9d4de3f0cd03f39748c88a90d5035f94844b9
lusine duryan <lusineduryan@mail.ru>
2019-10-07 14:40:40 +0400

    models built

---
+++ Stage_4/EF_CodeFirst_University/Dean.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CodeFirst_University
+{
+    public class Dean
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Faculty { get; set; }
+
+        public virtual ICollection<Lecturer> Lecturers { get; set; }
+        public virtual Rector Rector { get; set; }
+    }
+}
---
+++ Stage_4/EF_CodeFirst_University/Rector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CodeFirst_University
+{
+    public class Rector
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string University { get; set; }
+
+        public virtual ICollection<Dean> Deans { get; set; }
+    }
+}
