c8a0fa1e320eb06e1469f9103553d9c28981c165
lusine duryan <lusineduryan@mail.ru>
2019-10-10 17:12:26 +0400

    ll

--- Stage_4/EF_CodeFirst_University/Student.cs
+++ Stage_4/EF_CodeFirst_University/Student.cs
@@ -12,6 +12,7 @@ namespace EF_CodeFirst_University
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime? Birthday { get; set; }
