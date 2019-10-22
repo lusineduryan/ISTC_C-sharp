e57ea3c489ee7f3e5f6e735121267b2c1781b056
lusine duryan <lusineduryan@mail.ru>
2019-10-22 21:56:18 +0400

    tt

--- Stage_4/EF_CodeFirst_University/Dean.cs
+++ Stage_4/EF_CodeFirst_University/Dean.cs
@@ -11,7 +11,6 @@ namespace EF_CodeFirst_University
     public class Dean
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-
         public int Id { get; set; }
 
         [Required]
