2974b565f2f14b110b62e8a384cccb4529d0687c
lusine duryan <lusineduryan@mail.ru>
2019-11-04 23:16:02 +0400

    mw done

--- Stage_5/Middlewares/Startup.cs
+++ Stage_5/Middlewares/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 
 namespace Middlewares
 {
@@ -31,20 +33,15 @@ namespace Middlewares
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/{id1:int}/{id2:int}", async context =>
+                {
+                    var res = Convert.ToInt32(context.GetRouteValue("id1")) * Convert.ToInt32(context.GetRouteValue("id2"));
+                    await context.Response.WriteAsync(res.ToString());
+                });
             });
         }
     }
