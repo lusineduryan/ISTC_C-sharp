b184b8095fec842935e018111b2f93e45808e543
lusine duryan <lusineduryan@mail.ru>
2019-12-01 22:06:06 +0400

    pp

--- Stage_5/DependencyInjection/Startup.cs
+++ Stage_5/DependencyInjection/Startup.cs
@@ -38,6 +38,7 @@ namespace DependencyInjection
         {
             app.UseRouting();
 
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
