55d581666bdfc6d96a1d16cfe8a10e31992b988c
lusine duryan <lusineduryan@mail.ru>
2020-04-26 02:10:01 +0400

    mm

--- Stage_1/Homework_4_calculator/Program.cs
+++ Stage_1/Homework_4_calculator/Program.cs
@@ -13,7 +13,6 @@ namespace Homework_4_calculator
             Console.Write("Please enter the first number: ");
             double firstNumber = Convert.ToDouble(Console.ReadLine());
             Console.Write("\nPlease enter the second number: ");
-            
             double secondNumber = Convert.ToDouble(Console.ReadLine());
             Console.Write("\nPlease enter the operation type: ");
             string operationType = Console.ReadLine();
